cc6a779c0956b7ebe6aa999fa5b5c8ca2f325d76
Nevvord <nevvord@gmail.com>
2021-05-19 00:53:23 +0300

    remove dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-const port = 5000;
-app.get('/', (request, response) => {
-    response.send('Hello world!');
-});
-app.listen(port, () => console.log(`Running on port ${port}`));
