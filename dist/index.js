8a635b7c696851405383c3e2eac02d9637f71daa
Nevvord <nevvord@gmail.com>
2021-05-19 00:32:34 +0300

    express

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
-var num = 0;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 5000;
+app.get('/', function (request, response) {
+    response.send('Hello world!');
+});
+app.listen(port, function () { return console.log("Running on port " + port); });
